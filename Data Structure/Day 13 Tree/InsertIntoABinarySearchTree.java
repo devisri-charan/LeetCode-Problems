37b59ce3e5c225bc423f4b237e9e6d41271248b1
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2022-08-04 23:14:03 +0530

    New Problem

--- Data Structure/Day 13 Tree/InsertIntoABinarySearchTree.java
+++ Data Structure/Day 13 Tree/InsertIntoABinarySearchTree.java
@@ -25,6 +25,19 @@ public class InsertIntoABinarySearchTree {
     }
 
     public static TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null){
+            root = new TreeNode(val);
+            return root;
+        }
+
+        if (root.val > val){
+            root.left =  insertIntoBST(root.left, val);
+        }
+
+        else if (root.val < val){
+            root.right = insertIntoBST(root.right, val);
+        }
+
         return root;
     }
 }
