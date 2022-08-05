f42283d1fb3704643db10ccc318db3107342a2fe
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2022-08-05 18:47:18 +0530

    New Problem Solved

---
+++ Leetcode 75/Day 9 Graph-BFS-DFS/NumberOfIslands_.java
@@ -0,0 +1,16 @@
+public class NumberOfIslands_ {
+    public static void main(String[] args) {
+        char[][] grid = {
+                        {'1','1','1','1','0'},
+                        {'1','1','0','1','0'},
+                        {'1','1','0','0','0'},
+                        {'0','0','0','0','0'}
+                        };
+        System.out.println(numIslands(grid));
+    }
+
+    public static int numIslands(char[][] grid) {
+        int noOfIslands = 0;
+        return noOfIslands;
+    }
+}
