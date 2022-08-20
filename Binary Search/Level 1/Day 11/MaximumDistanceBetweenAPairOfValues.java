c8177ba7d3a2498ec1c5329c1a2a1a8466c3ee29
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2022-08-20 22:12:19 +0530

    Another Approach

--- Binary Search/Level 1/Day 11/MaximumDistanceBetweenAPairOfValues.java
+++ Binary Search/Level 1/Day 11/MaximumDistanceBetweenAPairOfValues.java
@@ -26,4 +26,22 @@ public class MaximumDistanceBetweenAPairOfValues{
         }
         return maxDistance;
     }
+
+    public static int maxDistance1(int[] nums1, int[] nums2) {
+        int maxDistance = 0;
+        int i = 0;
+        int j = 0;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        while (i < n1 && j < n2){
+            if (nums1[i] <= nums2[j]){
+                maxDistance = Math.max(maxDistance, j - i);
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return maxDistance;
+    }
 }
